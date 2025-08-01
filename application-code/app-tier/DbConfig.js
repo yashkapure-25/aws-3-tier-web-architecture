bb489333226c4e787ee6d9eba1531aa9e549474f
Yash Kapure <ypkapure74@gmail.com>
2025-08-01 14:08:58 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'YourPassword',
+    DB_DATABASE : 'webappdb'
+});
